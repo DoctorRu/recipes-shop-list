95d2debb34eb83605e5a0535f620e29c57e641a3
DoctorRu <vjblan@gmail.com>
2018-02-13 14:09:53 -0200

    Recipes: index

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,20 +1,30 @@
 import {Component} from '@angular/core';
 import {NavController} from "ionic-angular";
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
+import {Recipe} from "../../models/recipe";
+import {RecipeServices} from "../../services/recipes";
 
 @Component({
 	selector: 'page-recipes',
 	templateUrl: 'recipes.html',
 })
 export class RecipesPage {
+	public recipes: Recipe[];
 
 
-	constructor(private navCtrl: NavController){
+	constructor(private navCtrl: NavController,
+	            private recipesService: RecipeServices){
+	}
 
+	ionViewWillEnter(){
+		this.recipes = this.recipesService.getRecipes();
 	}
 
 	onNewRecipe(){
 		this.navCtrl.push(EditRecipePage, {mode: 'New'});
+	}
+
+	onLoadRecipe(){
 
 	}
 
